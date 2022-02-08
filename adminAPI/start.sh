08b3bdc5543a1898bd137664f82e9fb1c0e32479
Esua Ekpo <esuaekpo@yahoo.com>
2022-02-08 12:09:10 +0100

    fix test errors

--- adminAPI/start.sh
+++ adminAPI/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -xe
 
+python3 manage.py test
+
 python3 borrow_book.py &
 
 python3 register_user.py &
 
-python3 manage.py test
+python3 manage.py migrate
 
 python3 manage.py runserver 0.0.0.0:8000
--- clientAPI/start.sh
+++ clientAPI/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -xe
 
+python3 manage.py test
+
 python3 create_book.py &
 
 python3 delete_book.py &
 
-python3 manage.py test
+python3 manage.py migrate
 
 python3 manage.py runserver 0.0.0.0:9000
